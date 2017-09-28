e08cc7387000b8f18f1d7cd140a01126a4d5cf5c
Conor Nosal <cnosal@pivotal.io>
2017-09-28 14:53:22 -0400

    Add Razor views for retrieving Todo items

---
+++ TodoApi/Controllers/TodoViewController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("todo")]
+    public class TodoViewController : Controller
+    {
+        private readonly TodoService _service;
+        
+        public TodoViewController(TodoService service)
+        {
+            _service = service;
+        } 
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            IEnumerable<TodoItem> items = _service.GetAll().ToList();
+            ViewData["items"] = items;
+            return View();
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetById(long id)
+        {
+            var item = _service.GetById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            ViewData["item"] = item;
+            return View();
+        }
+    }
+}
